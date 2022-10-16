38b4854b489d0e2b7bf75484abeeda4cd01e84f6
Martin <marvahus@proton.me>
2022-10-16 18:58:03 +0200

    initial

---
+++ screen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ExtraScreen='HDMI-A-0'
+PrimaryScreen='eDP'
+
+if xrandr --query | grep "$ExtraScreen connected"
+then
+    xrandr \
+        --output $ExtraScreen --mode 1920x1080 --left-of $PrimaryScreen \
+        --output $PrimaryScreen --mode 1920x1080 --pos 0x0 --rotate normal --primary &
+    /home/martin/Doot.sh
+else
+    xrandr \
+        --output $ExtraScreen --off \
+        --output $PrimaryScreen --mode 1920x1080 --pos 0x0 --rotate normal &
+fi
